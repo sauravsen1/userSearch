86260bc1395848ed7735481aff9cc3a3fe5f33f7
sauravsen1 <sauravsencse3@gmail.com>
2017-08-10 13:24:42 +0600

    add all new

--- mySearch/js/controllers.js
+++ mySearch/js/controllers.js
@@ -1,29 +1,32 @@
-var artistControllers = angular.module('artistControllers', ['ngAnimate']);
+var artistControllers = angular.module('artistControllers', []);
 
 artistControllers.controller('ListController', ['$scope', '$http', function($scope, $http) {
-  $http.get('js/data.json').success(function(data) {
-    $scope.artists = data;
-    $scope.artistOrder = 'name';
+  $http.get("js/data.json")
+  .then(function(response) {
+      $scope.artists = response.data;
+      $scope.artistOrder = 'name';
   });
 }]);
 
 artistControllers.controller('DetailsController', ['$scope', '$http','$routeParams', function($scope, $http, $routeParams) {
-  $http.get('js/data.json').success(function(data) {
-    $scope.artists = data;
-    $scope.whichItem = $routeParams.itemId;
+  $http.get("js/data.json")
+  .then(function(response) {
+      $scope.artists = response.data;
+      $scope.whichItem = $routeParams.itemId;
+      console.log($routeParams.itemId);
+      
+      if ($routeParams.itemId > 0) {
+        $scope.prevItem = Number($routeParams.itemId)-1;
+      } else {
+        $scope.prevItem = $scope.artists.length-1;
+      }
 
-    if ($routeParams.itemId > 0) {
-      $scope.prevItem = Number($routeParams.itemId)-1;
-    } else {
-      $scope.prevItem = $scope.artists.length-1;
-    }
-
-    if ($routeParams.itemId < $scope.artists.length-1) {
-      $scope.nextItem = Number($routeParams.itemId)+1;
-    } else {
-      $scope.nextItem = 0;
-    }
-
+       if ($routeParams.itemId < $scope.artists.length-1) {
+        $scope.nextItem = Number($routeParams.itemId)+1;
+      } else {
+        $scope.nextItem = 0;
+      }
   });
 }]);
+
 
